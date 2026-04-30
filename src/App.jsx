d2f36bab5a76d6595874fdcc78f91be05f6bb01f
Abhishek Peiris <abhisheklpeiris@gmail.com>
2026-04-30 12:43:07 +0530

    PROJECT MILESTONES

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-import Milestones from './components/Milestones'
 import Resources from './components/Resources'
 import Technologies from './components/Technologies'
 import AboutUs from './components/AboutUs'
@@ -14,6 +13,7 @@ import ResearchGap from "./components/research_gap/ResearchGap"
 import ProblemSolution from "./components/problem_solution/ProblemSolution"
 import ResearchObjectives from "./components/objectives/ResearchObjectives"
 import Methodology from "./components/methodology/Methodology"
+import Milestones from './components/milestones/Milestones'
 
 
 const sectionVariants = {
