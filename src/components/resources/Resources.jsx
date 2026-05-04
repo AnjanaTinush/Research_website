0f2c2004416100a636ff642b228f2b0e55cb367b
Anjana Tinush <146495563+AnjanaTinush@users.noreply.github.com>
2026-05-04 18:16:49 +0530

    add repsots

--- src/components/resources/Resources.jsx
+++ src/components/resources/Resources.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { FileText, Presentation } from "lucide-react";
 import bannerPdf from "../../assets/images/Banner.pdf";
+import { li } from "framer-motion/client";
 
 const ResourceCard = ({
   title,
@@ -67,11 +68,13 @@ const Resources = () => {
       title: "Topic Assessment",
       date: "2024/01/19",
       type: "Group",
+      link: "https://drive.google.com/drive/folders/1X0PlANKRfj-qcTC6x8tZ3GDpB_Vt75Di?usp=sharing",
     },
     {
       title: "Project Proposal",
       date: "2024/02/29",
       type: "Individual",
+      link: "https://drive.google.com/drive/folders/1A1ZCjQZfKo5BOnGn9ysYmKBxpIXbvgSj?usp=sharing",
     },
     {
       title: "Status Documents I",
@@ -92,6 +95,7 @@ const Resources = () => {
       title: "Final Reports",
       date: "2024/12/01",
       type: "Group & Individual",
+      link: "https://drive.google.com/drive/folders/1wGys-Whal3gFJpKurMqCtbI-66KhcVtD?usp=sharing",
     },
     {
       title: "Poster",
@@ -122,7 +126,7 @@ const Resources = () => {
       date: "2024/09/11",
       type: "Group",
       action: "View",
-      link: "https://docs.google.com/presentation/d/1__2OcXygy2pTgBTpXgVTFw6HlKWM5sgF/edit?usp=sharing&ouid=106479244210891845492&rtpof=true&sd=true",
+      link: "https://docs.google.com/presentation/d/1hiYVmHnCGNiI4cs-rweB1g4iggjfw7Lr/edit?usp=sharing&ouid=106479244210891845492&rtpof=true&sd=true",
     },
     {
       title: "Final Presentation",
